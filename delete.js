0d4bcb7d176ea891ee5a012b9a9975dccd27df0d
laura carreno <lauracarreno336@gmail.com>
2024-05-03 23:01:59 -0600

    nuevocommit

---
+++ delete.js
@@ -0,0 +1,6 @@
+const deleteButton = document.querySelector(".button-delete")
+borrar.addEventListener("click", function() {
+    listafunc.remove()
+})
+//borrar.addEventListener("click", function() {
+//listafunc.removeChild(listafunc.lastChild)//E
